594b01b6f17109fb9c85da8eefca9091d99c85e9
nairsajjalwork <nairsajjal.work@gmail.com>
2024-09-08 16:01:42 +0530

    Initial Commit

---
+++ testCICD/testCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCICDApp.swift
+//  testCICD
+//
+//  Created by Sajjal Nair on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct testCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
